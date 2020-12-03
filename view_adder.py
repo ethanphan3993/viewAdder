d9eb015ebe0dc92f973abd5404c0729190e73d71
Ethan Phan <ethanphan3993@gmail.com>
2020-12-03 20:26:38 +1000

    Opening 2 tabs

--- view_adder.py
+++ view_adder.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+import time
 fileName = "videoList.txt"
 videoFile = open(fileName)
 listVideos = videoFile.readlines();
@@ -7,12 +7,20 @@ listVideos = videoFile.readlines();
 buttonPlaySelector = "#movie_player > div.ytp-cued-thumbnail-overlay > button"
 
 # open browser
-browser = webdriver.Chrome("/Users/ethanphan/Documents/Personal Project/ViewAdding/chromedriver")
+browser = webdriver.Chrome("/Users/ethanphan/Documents/Personal Project/ViewAdder/chromedriver")
 
 index = 0
 
 #open url
 browser.get(listVideos[index])
-
+time.sleep(1)
 playButton = browser.find_element_by_css_selector(buttonPlaySelector)
 playButton.click()
+
+
+# Open new tab
+time.sleep(0.5)
+index = index + 1
+js = "window.open('"+listVideos[index].strip()+"')"
+browser.execute_script(js)
+
